5ca39d921b54a64e1523f9654309ed8603501304
yngtdd <yngtdd@gmail.com>
2023-05-23 13:05:35 -0400

    refactor: Clean up a bit

--- flake.nix
+++ flake.nix
@@ -3,14 +3,15 @@
   
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    home-manager.url = "github:nix-community/home-manager/release-22.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-22.11";
+      inputs.nixpkgs.follows = "nixpkgs";      
+    };
   };
 
   outputs = { nixpkgs, home-manager, ... }: 
   let 
     system = "x86_64-linux";
-    
     pkgs = import nixpkgs {
       inherit system;
       config = { allowUnfree = true; };
