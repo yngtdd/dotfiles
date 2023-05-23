a64a905c94a17b46e69566ba62d11e512aa85481
yngtdd <yngtdd@gmail.com>
2023-05-23 12:36:36 -0400

    feat: Add Spotify

--- apply-user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pushd ~/.dotfiles
-nix build .#homeManagerConfigurations.yngtdd.activationPackage
-./result/activate
-popd
--- system/configuration.nix
+++ system/configuration.nix
@@ -86,7 +86,9 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    helix
     firefox
+    spotify
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
